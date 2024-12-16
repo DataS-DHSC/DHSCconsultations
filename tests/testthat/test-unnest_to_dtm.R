33f8f97540e9aac49d390549358771d7f8ffc6d2
James Riley <James.Riley@dhsc.gov.uk>
2024-12-16 11:51:33 +0000

    Test roughly based off tidytext's test for this function

---
+++ tests/testthat/test-unnest_to_dtm.R
@@ -0,0 +1,24 @@
+test_that("unnest_to_dtm works", {
+  # simple cleaning function for testing purposes
+  clean_words <- function(x) {
+    x |>
+      stringr::str_to_lower() |>
+      stringr::str_replace_all("[^[:alnum:]]", " ") |>
+      stringr::str_squish()
+  }
+
+  # dummy response data
+  d1 = unnest_words(
+    dummy_response,
+    "Please share your views on policy 1",
+    clean_words, "", tidytext::stop_words$word, ""
+  ) %>%
+    unnest_to_dtm("response_id", "word")
+
+  # is the right class
+  expect_s3_class(d1, "DocumentTermMatrix")
+
+  # is the right dimensions
+  expect_equal(dim(d1), c(100, 3707))
+
+})
